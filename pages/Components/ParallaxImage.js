a87f29f7f9384b5c680c5480b076c8b4335af85a
Raven The wolf <theoccultcorner@gmail.com>
2023-04-02 21:39:37 -0700

    fFD

--- pages/Components/ParallaxImage.js
+++ pages/Components/ParallaxImage.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+ 
 import { Parallax } from 'react-parallax';
 
-const useStyles = makeStyles((theme) => ({
  
- 
-}));
 
 function ParallaxImage({ imageUrl }) {
-  const classes = useStyles();
+ 
 
   return (
     <Parallax bgImage={imageUrl} strength={500}>
-      <div className={classes.parallaxContainer}>
-        <div className={classes.title}>
+      <div>
+        <div>
           Earl Brothers Masonry
         </div>
       </div>
